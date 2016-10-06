95a8a5fc08899f33a3ab478195916774db36d062
odulzaides <dulzaides.oscar@gmail.com>
2016-10-06 16:34:13 -0400

    Various Virtual envs on local machine.

---
+++ Django-1.3.7/build/scripts-2.7/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Tuti/Google Drive/Code_and_Code_examples/Python/Virtualenv/django13/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ django_1.04/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Tuti/Coding/Python/Virtualenv/django_1.04/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
